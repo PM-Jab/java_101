5d151a2aee492bb08b0a3ebc700bc99ce78b662f
Possawee Muttisan <possawee.m@arise.tech>
2024-07-19 00:36:17 +0700

    primitive types

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int num = 12;
+        System.out.println(num + 10);
+
+        float numf = 5.6f;
+        System.out.println(numf + 10.0f);
+
+        byte numb = 6;
+        System.out.println(numb + 3.0);
+    }
+}
