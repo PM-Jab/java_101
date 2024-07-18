bc68a6cda3b3d5d3ce31f05aac06b685545a22b2
Possawee Muttisan <possawee.m@arise.tech>
2024-07-19 01:46:25 +0700

    if else

---
+++ condition/Condition.java
@@ -0,0 +1,14 @@
+package condition;
+
+public class Condition {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a >= 30) {
+            System.out.println("More than 30");
+        } else if (a >= 20) {
+            System.out.println("More than 20");
+        } else {
+            System.out.println("Less than 20");
+        }
+    }
+}
