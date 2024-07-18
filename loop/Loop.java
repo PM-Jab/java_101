0568ad503b49b2249f33a862ef1a03e8ad2e97eb
Possawee Muttisan <possawee.m@arise.tech>
2024-07-19 02:50:42 +0700

    nest loop

--- loop/Loop.java
+++ loop/Loop.java
@@ -5,11 +5,25 @@ public class Loop {
         int i = 5;
 
         while_loop(i);
+
+        nest_loop(i);
     }
 
     public static void while_loop(int i) {
         while (i > 0) {
             System.out.println("Count down: " + i);
+            --i;
+        }
+    }
+
+    public static void nest_loop(int i) {
+        int j = 3;
+        while (i > 0) {
+            System.out.println("Loop i: " + i);
+            while (j > 0) {
+                System.out.println("Loop j: " + j);
+                --j;
+            }
             --i;
         }
     }
