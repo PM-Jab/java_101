447f1f50f927c186f4c1bd589e4f6bc71bd3ffd9
Possawee Muttisan <possawee.m@arise.tech>
2024-07-19 02:13:23 +0700

    while loop

--- loop/Loop.java
+++ loop/Loop.java
@@ -1,5 +1,16 @@
 package loop;
 
 public class Loop {
+    public static void main(String[] args) {
+        int i = 5;
 
+        while_loop(i);
+    }
+
+    public static void while_loop(int i) {
+        while (i > 0) {
+            System.out.println("Count down: " + i);
+            --i;
+        }
+    }
 }
